bd03b6a2d3667dc50b4a5b031269eb5da06dae3f
Rishitha <trishithapandu2218@gmail.com>
2024-12-10 22:44:46 +0530

    Add files via upload

---
+++ Foreach.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Foreach{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String a[]={"Apple","Orange","Pineapple"};
+        for(String b:a)
+        System.out.println(b+" , ");
+    }
+}
