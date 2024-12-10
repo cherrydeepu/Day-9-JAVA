8268abf6e76e3f4c525cd13e3aca21b4beb1af3d
Rishitha <trishithapandu2218@gmail.com>
2024-12-10 23:31:52 +0530

    Add files via upload

---
+++ nextandline.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class nextandline{
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    String a=s.next();
+    System.out.println(a);
+    String b=s.nextLine();
+    System.out.println(b);
+  }
+}
